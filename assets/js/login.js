fd848d012474484434c2e62d5061b059a04ed595
iagosoares0212 <iagosoares55@gmail.com>
2024-09-27 16:46:12 -0300

    .

---
+++ assets/js/login.js
@@ -0,0 +1,17 @@
+document.getElementById('LoginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o comportamento padrão de recarregar a página
+
+    // Aqui você pode adicionar a lógica de validação de login se necessário
+    // Exemplo: verificar se o email ou senha não está vazio
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Exemplo básico de validação (pode ser mais complexo)
+    if (email !== '' && password !== '') {
+        // Redireciona para a página desejada após o login
+        window.location.href = '/pages/index.html'; // Substitua pelo link da página que deseja redirecionar
+    } else {
+        alert('Por favor, preencha todos os campos!');
+    }
+});
